2ef585e8693dd4989b4c34b682e9d0486defd9cc
liumapp <liumapp.com@gmail.com>
2018-06-05 09:06:19 +0800

    convert doc to pdf

--- src/main/java/com/liumapp/convert/doc/controller/IndexController.java
+++ src/main/java/com/liumapp/convert/doc/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.liumapp.convert.doc.controller;
 
+import com.liumapp.convert.doc.Doc2PDF;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("")
 public class IndexController {
 
+    private String path = "/Users/docker/add-mandarin-fonts-in-docker/";
+
     @RequestMapping("")
     public void index () {
         System.out.println("this is index controller ,ready to convert test doc into pdf ");
+
+        Doc2PDF doc2PDF = new Doc2PDF();
+        doc2PDF.doc2pdf(path + "/test.pdf", path + "doc/test.doc");
     }
 
 }
