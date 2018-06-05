d1dd81dd52d2a96443cb22d4fc2ccfb3d7ed65c5
liumapp <liumapp.com@gmail.com>
2018-06-05 09:02:53 +0800

    add springboot starter

---
+++ src/main/java/com/liumapp/convert/doc/Main.java
@@ -0,0 +1,20 @@
+package com.liumapp.convert.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liumapp
+ * @file Main.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/5/18
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main (String[] args) {
+        SpringApplication.run(Main.class , args);
+    }
+
+}
