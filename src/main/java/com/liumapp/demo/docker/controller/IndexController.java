b7a651fd2bc437d919e10edd3be1faa7f26d7691
liumapp <liumapp.com@gmail.com>
2018-06-05 10:07:21 +0800

    add wrong pdf

--- src/main/java/com/liumapp/demo/docker/controller/IndexController.java
+++ src/main/java/com/liumapp/demo/docker/controller/IndexController.java
@@ -15,14 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("")
 public class IndexController {
 
-    private String path = "/pdf/";
+    /**
+     * 只能在docker下运行正常，如果要在idea调试，请确保宿主机doc和pdf目录存在，并拥有test.doc
+     */
+    private String pdfPath = "/pdf/";
+
+    private String docPath = "/doc/";
 
     @RequestMapping("")
     public void index () {
         System.out.println("this is index controller ,ready to convert test doc into pdf ");
 
         Doc2PDF doc2PDF = new Doc2PDF();
-        doc2PDF.doc2pdf(path + "/test.pdf", path + "doc/test.doc");
+        doc2PDF.doc2pdf(pdfPath + "test.pdf", docPath + "test.doc");
     }
 
 }
